27690672fb0026995e0528530a9846a3f08d127e
DeogenesMaranan <maranandeogenes@gmail.com>
2024-06-19 18:41:24 -0800

    create download button for project html

--- project.py
+++ project.py
@@ -11,4 +11,15 @@ class ProjectManager:
         elif not os.path.exists(back_path):
             return redirect(url_for('show_layout', project_name=project_name, type='back'))
         else:
-            return render_template('project.html')
+            
+            return render_template('project.html', front_path=front_path, back_path=back_path, project_name=project_name)
+
+    @app.route('/download/<project_name>/<type>', methods=['GET'])
+    def download_layout(project_name, type):
+        layout_path = os.path.abspath(os.path.join('projects', project_name, f'{type}_layout.xlsx'))
+        
+        if type == 'front' or type == 'back':
+            if os.path.exists(layout_path):
+                return send_file(layout_path, as_attachment=True)
+        
+        return jsonify({'error': 'File not found'}), 404
